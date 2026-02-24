629a21301fba7fbb200cc94861d0ba0261b6f65d
gabrielboniolo <gabrielboniolo@gmail.com>
2026-02-24 06:12:57 -0300

    feat: dependencies file

--- auth_routers.py
+++ auth_routers.py
@@ -1,6 +1,6 @@
-from fastapi import APIRouter
-from models import Usuario, db
-from sqlalchemy.orm import sessionmaker
+from fastapi import APIRouter, Depends
+from models import Usuario
+from dependencies import pegar_sessao
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -10,9 +10,9 @@ async def auth():
     return {"mensagem": "Você acessou a rota de autenticação.", "autenticado": False}
 
 @auth_router.post("/criar_conta")
-async def criar_conta(nome:str, email:str, senha:str):
-    Session = sessionmaker(bind=db)
-    session = Session()
+async def criar_conta(nome:str, email:str, senha:str, session = Depends(pegar_sessao)):
+    """ Rota para a criação da conta """
+
     usuario = session.query(Usuario).filter(Usuario.email==email).first()
 
     if usuario:
