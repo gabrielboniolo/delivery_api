76bc77fa9eadbd08d4ed18c77153d5277a031f19
gabrielboniolo <gabrielboniolo@gmail.com>
2026-03-02 05:41:46 -0300

    feat: post method and pydantic schemas for auth and order

--- auth_routers.py
+++ auth_routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import Usuario
 from dependencies import pegar_sessao
 from main import bcrypt_context
+from schemas import UsuarioSchema
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -11,16 +12,25 @@ async def auth():
     return {"mensagem": "Você acessou a rota de autenticação.", "autenticado": False}
 
 @auth_router.post("/criar_conta")
-async def criar_conta(nome:str, email:str, senha:str, session = Depends(pegar_sessao)):
-    """ Rota para a criação da conta """
+async def criar_conta(usuario_schema:UsuarioSchema, session = Depends(pegar_sessao)):
+    """ 
+    Rota para a criação da conta 
 
-    usuario = session.query(Usuario).filter(Usuario.email==email).first()
+    """
+
+    usuario = session.query(Usuario).filter(Usuario.email==usuario_schema.email).first()
 
     if usuario:
         raise HTTPException(status_code=400, detail="Usuário já cadastrado")
     else:
+        nome = usuario_schema.nome
+        email = usuario_schema.email
+        senha = usuario_schema.senha
+
         senha_criptografada = bcrypt_context.hash(senha)
+
         novo_usuario = Usuario(nome, email, senha_criptografada)
+
         session.add(novo_usuario)
         session.commit()
         return {"mensagem": f"Usuário cadastrado com sucesso {email}."}
--- order_routers.py
+++ order_routers.py
@@ -1,12 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from dependencies import pegar_sessao
+from schemas import PedidoSchema
+from models import Pedido
 
-order_router = APIRouter(prefix="/order", tags=["order"])
+order_router = APIRouter(prefix="/pedidos", tags=["pedidos"])
 
 @order_router.get("/")
-async def order():
-    """
-      Rota padrão de pedidos.
-      Todas as rotas e pedidos precisam de autenticação.
+async def pedidos():
+  """
+  Rota padrão de pedidos.
+  Todas as rotas e pedidos precisam de autenticação.
 
-    """
-    return {"mensagem": "Você acessou a rota de pedidos."}
+  """
+  return {"mensagem": "Você acessou a rota de pedidos."}
+
+@order_router.post("/pedido")
+async def criar_pedido(pedido_schema: PedidoSchema, session : Session  = Depends(pegar_sessao)):
+  """  
+  Rota para a criação de pedidos.
+    
+  """
+
+  novo_pedido = Pedido(usuario=pedido_schema.id_usuario)
+
+  session.add(novo_pedido)
+  session.commit()
+
+  return {"mensagem": f"O pedido foi criado com sucesso. ID do pedido: {novo_pedido.id}"}
