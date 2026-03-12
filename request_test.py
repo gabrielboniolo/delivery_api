26ecb25028635e90b25762f3c160bfdf232605a4
gabrielboniolo <gabrielboniolo@gmail.com>
2026-03-12 08:05:49 -0300

    fix: header key name (authorization)

--- request_test.py
+++ request_test.py
@@ -1,14 +1,12 @@
 import requests
 
-refresh_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." \
-"eyJzdWIiOjQsImV4cGlyYXRpb25fZGF0ZSI6IjIwMjYtMDMtMTggMTA6MDg6NDMuMTAwODExKzAwOjAwIn0." \
-"SRaAqjCN5SywjZTULifPLymLA198896QxxKHpUyDFvg"
+refresh_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI0IiwiZXhwaXJhdGlvbl9kYXRlIjoiMjAyNi0wMy0xOSAwOToxNzo0Ni4yNjE2NTkrMDA6MDAifQ.fIur6I9TGSa9Zq3d1YYosNI5wZRiIJDagGLZmLIWluI"
 
 headers = {
-    "Authorize": f"Bearer {refresh_token}"
+    "Authorization": f"Bearer {refresh_token}"
 }
 
 response = requests.get("http://127.0.0.1:8000/auth/refresh",headers=headers)
 
 print(response)
-print(response.json())
+# print(response.json())
